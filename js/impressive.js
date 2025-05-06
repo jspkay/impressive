8f802a4ebfdb2a4acc798c93f800d7f5d87df1e6
spappalardo <jspkay@gmail.com>
2025-05-06 17:42:37 +0200

    Skeleton is almost done

--- js/impressive.js
+++ js/impressive.js
@@ -1,17 +1,25 @@
 "use strict";
 
+const Modes = {
+  EDITOR: 0,
+  PRESENTATION: 1,
+};
+
 // DOM Management 
 (function(document, window){ 
-  "use strict";
   console.log("Started");
 
   var impressive = window.impressive = function(rootId){
 
     var tools;
+    var activeMode = Modes.EDITOR;
+
+    var sm = new StepManager();
 
     var init = function(){
 
       tools = initTools(rootId);
+  
 
       var activeTool = tools.ContainerTool;
       activeTool.init();
@@ -39,6 +47,9 @@
               activeTool.destroy();
               activeTool = tools.ContainerTool;
             break;
+            case "presentTool":
+              activeMode = Modes.PRESENTATION;
+              switchMode(activeMode);
           }
           activeTool.init();
 
@@ -46,18 +57,27 @@
       })
     }
 
+    function switchMode(mode){
+      if(mode == Modes.PRESENTATION){
+        document.querySelector("#overlay").classList.add("hidden");
+      }
+      if(mode == Modes.EDITOR){
+
+      }
+    }
+
     return ({
       init: init,
     });
   }
 
-    // ADD and INIT LIBRARIES
-    // Library factories are defined in src/lib/*.js, and register themselves by calling
-    // impress.addLibraryFactory(libraryFactoryObject). They're stored here, and used to augment
-    // the API with library functions when client calls impress(rootId).
-    // See src/lib/README.md for clearer example.
-    // (Advanced usage: For different values of rootId, a different instance of the libaries are
-    // generated, in case they need to hold different state for different root elements.)
+// ADD and INIT LIBRARIES
+// Library factories are defined in src/lib/*.js, and register themselves by calling
+// impress.addLibraryFactory(libraryFactoryObject). They're stored here, and used to augment
+// the API with library functions when client calls impress(rootId).
+// See src/lib/README.md for clearer example.
+// (Advanced usage: For different values of rootId, a different instance of the libaries are
+// generated, in case they need to hold different state for different root elements.)
   var toolFactories = {};
   impressive.addToolFactory = function( obj ) {
       for ( var toolname in obj ) {
@@ -83,6 +103,48 @@
   };
 
 })(document, window);
+
+class StepManager {
+  constructor(){
+    this.root = document.querySelector("#stepList");
+    this.last = document.querySelector("#addStep");
+    let addButton = document.querySelector("#addStep");
+    addButton.addEventListener(
+      "click", (event ) => {
+        console.log(this);
+        let scene = new Canvas();
+        let x = Number(scene.dataset.x);
+        let y = Number(scene.dataset.y);
+        let scale = 1;
+        this.createStep(Array(x,y,scale));
+        console.log("New step!");
+      }
+    )
+  }
+  createStep(pos){
+    let step = document.createElement("div");
+    step.classList.add("step");
+    step.dataset.x = pos[0];
+    step.dataset.y = pos[1];
+    step.dataset.scale = pos[2];
+    step.addEventListener(
+      "click", 
+      (e) => {
+        console.log(e.target);
+        let el = e.target;
+        let scene = new Canvas();
+
+        let x = el.dataset.x;
+        let y = el.dataset.y;
+        let scale = el.dataset.scale;
+
+        scene.move(x, y, scale);
+      }
+    )
+    this.last.insertAdjacentElement("beforebegin", step);
+    console.log(pos);
+  }
+}
 
 // Mouse info
 class Mouse{
@@ -147,6 +209,8 @@ class Mouse{
       setPosition(x, y){
         console.log("setting Position to ", x, y);
         this.element.style.transform = `translate(${x}px, ${y}px)`;
+        this.element.dataset.x = x;
+        this.element.dataset.y = y;
       }
       getPosition(){
         let style = window.getComputedStyle(this.element);
@@ -155,7 +219,7 @@ class Mouse{
           matrix = "matrix(1, 0, 0, 1, 0, 0)";
         }
         let values = matrix.replace(/\)/g, "").replace(/,/g, "").split(" ");
-        console.log(values);
+        // console.log(values);
         let x = Number(values[4]);
         let y = Number(values[5]);
         return [x, y];
@@ -173,10 +237,12 @@ class Mouse{
       document.querySelector("#moveTool").classList.add("active");
       var mouse = new Mouse();
       let f;
+      let initialPos = Array()
 
       f = (e) => {
         mouse.mouseDown(e);
         active = new MainBody(rootId);
+        initialPos = active.getPosition();
       };
       document.addEventListener("mousedown", f);
       eventListeners["mousedown"] = f;
@@ -184,11 +250,11 @@ class Mouse{
       f = (e) => {
         mouse.mouseMove(e);
         if(mouse.clicking){
-          console.log("moving...");
           let dx = mouse.currentCoord[0] - mouse.clickStarted[0];
           let dy = mouse.currentCoord[1] - mouse.clickStarted[1];
           console.log(dx, dy);
-          active.setPositionDelta(dx, dy);
+          // active.setPositionDelta(dx, dy);
+          active.setPosition( initialPos[0] + dx , initialPos[1] + dy )
         }
       };
       document.addEventListener("mousemove", f);
@@ -220,6 +286,17 @@ class Mouse{
 
 })(document, window);
 
+class Canvas{
+  constructor(){
+   this.element = document.querySelector("#scene");
+   this.dataset = this.element.dataset;
+  }
+  move(x, y, scale){
+    let str = `translate(${x}px,${y}px) scale(${scale})`;
+    this.element.style.transform = str; 
+    console.log(str)
+  }
+}
 
 function round(x, m){
   let r = x / m;
@@ -238,12 +315,12 @@ class Element{
     this.y = Number(element.dataset.y);
     this.tooSmall = false; // Does this stay ?
   }
-  setPosition(x, y){
-    this.element.style.transform = `translate(${x}px, ${y}px)`;
-    x = round(x, M); // Compute position 
-    y = round(y, M);
-    this.x = x;
-    this.y = y;
+  setPosition(x, y, sticky = false){
+    if(sticky) {
+      x = round(x, M); // Compute position 
+      y = round(y, M);
+    }
+    this._setPosition(x, y);
   }
   getProperties(){
     return {
@@ -254,18 +331,28 @@ class Element{
     this.element.remove();
     delete this.element;
   }
+  _setPosition(x, y){
+    this.element.style.transform = `translate(${x}px, ${y}px)`;
+    this.x = x;
+    this.y = y;
+  }
 }
 
 class Container extends Element{
   constructor(element){
-    super(element)
+    super(element);
+  }
+  static tranfromWrtRoot(x, y){
+    let dataset = document.querySelector(`#${rootId}`).dataset;
+    x = x - Number( dataset.x );
+    y = y - Number( dataset.y );
+    return [x, y];
   }
   static create(x, y){
     let element = document.createElement("div"); // creating container
     element.classList.add("container"); // appropriate style
     element.style.position = "absolute"; // prevents overlapping
-    x = round(x, M); // Compute position 
-    y = round(y, M);
+    [x, y] = Container.tranfromWrtRoot(x, y);
     element.dataset.x = x; // position is redundant, for easy retrival
     element.dataset.y = y;
     element.style.transform = `translate(${x}px, ${y}px)`; // position the element on display
@@ -282,11 +369,10 @@ class Container extends Element{
     this.element.style.width = `${w}px`;
   }
   setSizeFromPos(px, py){
+    [px, py] = Container.tranfromWrtRoot(px, py);
     let w = px - this.x;
     let h = py - this.y;
-    this.tooSmall = h < M || w < M;
-    w = round(w, M);
-    h = round(h, M);
+    this.tooSmall = h < 0 || w < 0;
     this.setSize(w,h);
   }
   setFillColor(color){
@@ -296,6 +382,10 @@ class Container extends Element{
     this.element.style.borderColor = color;
   }
   setBorderThickness(t){
+    let old = Number( this.element.style.borderWidth.replace("px", "") )
+    let displacement = t - old;
+    console.log(displacement);
+    this._setPosition( this.x - displacement, this.y - displacement);
     this.element.style.borderWidth = `${t}px`;
     this.element.style.borderStyle = "solid";
   }
@@ -332,8 +422,6 @@ class PropertyWindow extends Container{
     let element = document.createElement("div"); // creating container
     element.classList.add("property-window"); // appropriate style
     element.style.position = "absolute"; // prevents overlapping
-    x = round(x, M); // Compute position 
-    y = round(y, M);
     element.dataset.x = x; // position is redundant, for easy retrival
     element.dataset.y = y;
     element.style.transform = `translate(${x}px, ${y}px)`; // position the element on display
@@ -377,6 +465,7 @@ class PropertyWindow extends Container{
         mouse.mouseDown(e);
         active = Container.create(mouse.clickStarted[0], mouse.clickStarted[1]);
         active.tooSmall = true;
+        console.log(e)
       };
       document.addEventListener("mousedown", f);
       eventListeners["mousedown"] = f;
@@ -384,6 +473,7 @@ class PropertyWindow extends Container{
       f = (e) => {
         mouse.mouseMove(e);
         if(mouse.clicking) active.setSizeFromPos(mouse.currentCoord[0], mouse.currentCoord[1]);
+        console.log(e);
       };
       document.addEventListener("mousemove", f);
       eventListeners["mouseup"] = f;
