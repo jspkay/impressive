34f949b932dca3eba8f3d9e9abb53236b980deff
spappalardo <jspkay@gmail.com>
2025-09-01 00:53:16 +0200

    Added interact.js

--- browserify.js
+++ browserify.js
@@ -1,4 +1,8 @@
+// GoldenLayout
 var GL = require("golden-layout");
 
 var GoldenLayout = GL.GoldenLayout;
 window.GoldenLayout = GoldenLayout;
+
+// interact
+window.interact = require("interactjs");
